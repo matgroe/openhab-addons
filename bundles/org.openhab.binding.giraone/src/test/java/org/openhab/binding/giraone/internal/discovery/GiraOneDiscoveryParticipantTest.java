c66c9f207fdfcbdbd6de524b31111739279934d0
matthias <matgroe@gmail.com>
2025-01-25 16:02:46 +0000

    Initial commit

---
+++ bundles/org.openhab.binding.giraone/src/test/java/org/openhab/binding/giraone/internal/discovery/GiraOneDiscoveryParticipantTest.java
@@ -0,0 +1,25 @@
+package org.openhab.binding.giraone.internal.discovery;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GiraOneDiscoveryParticipantTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void getSupportedThingTypeUIDs() {
+    }
+
+    @Test
+    void createResult() {
+    }
+
+    @Test
+    void getThingUID() {
+    }
+}
